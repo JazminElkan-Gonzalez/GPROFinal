35840e0659dd6b9b4bf0b3be51178f1c434f3e37
Alex Adkins <adkins.alexandra@gmail.com>
2014-12-17 20:38:55 -0500

    ADD: Statue image

--- status.py
+++ status.py
@@ -6,7 +6,5 @@ from thing import *
 class OlinStatue (Thing):
     def __init__ (self,name,desc, health):
         Thing.__init__(self,name,desc, health)
-        rect = Rectangle(Point(1,1),Point(TILE_SIZE-1,TILE_SIZE-1))
-        rect.setFill("gray")
-        rect.setOutline("gray")
-        self._sprite = rect
+        self._pic = 'statue.gif'
+        self._sprite = Image(Point(TILE_SIZE/2,TILE_SIZE/2),self._pic)
